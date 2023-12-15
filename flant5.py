991a1dc1d4d234c4307d43f7d5598ab479460f47
kaikaic1998 <kainianc@gmail.com>
2023-12-15 20:04:15 +0900

    modify FLAN T5 code

--- flant5.py
+++ flant5.py
@@ -1,10 +1,20 @@
-from transformers import AutoModelForSeq2SeqLM
-from transformers import AutoTokenizer
-from transformers import GenerationConfig
+# from transformers import AutoModelForSeq2SeqLM
+# from transformers import AutoTokenizer
+# from transformers import GenerationConfig
 
-model_name='google/flan-t5-base'
+# model_name='google/flan-t5-base'
 
-model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+# model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
-tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+# tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
 
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base")
+model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-base", device_map="auto")
+
+input_text = "translate English to German: How old are you?"
+input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cuda")
+
+outputs = model.generate(input_ids)
+print(tokenizer.decode(outputs[0]))
