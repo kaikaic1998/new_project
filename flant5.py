26d5d2460c8ccf06fbc9e4648707fb592f58ecbd
kaikaic1998 <kainianc@gmail.com>
2023-12-16 10:26:00 +0900

    playing around FLAN T5

--- flant5.py
+++ flant5.py
@@ -13,7 +13,7 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base")
 model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-base", device_map="auto")
 
-input_text = "translate English to German: How old are you?"
+input_text = "where is London?"
 input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cuda")
 
 outputs = model.generate(input_ids)
